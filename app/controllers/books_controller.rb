023045ee15cd4f5f7bf8c302be768e3db52e10f7
EC2 Default User <miyabi.kanjani@ezweb.ne.jp>
2021-11-04 04:24:32 +0000

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def edit
+  end
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
