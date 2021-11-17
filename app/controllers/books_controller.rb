5f696e1c5bc141b465c20b5af5e935cd7b6abebb
EC2 Default User <miyabi.kanjani@ezweb.ne.jp>
2021-11-17 14:20:39 +0000

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,6 +34,23 @@ class BooksController < ApplicationController
   end
   end
 
+  def update
+    @book= Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path,notice: 'You have updated book successfully.'
+    else
+      @user = current_user
+      render :edit
+    end
+  end
+
+  def destroy
+  @book = Book.find(params[:id])
+  @book.destroy
+  redirect_to books_path
+  end
+
+
   private
     def book_params
       params.require(:book).permit(:title,:body)
